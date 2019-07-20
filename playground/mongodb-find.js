005b494915f8f67d579419cd8142c15a41ed3058
unknown <amitbishnoi.brw@gmail.com>
2019-07-20 16:29:46 +0530

    fetching the database

---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
+
+    if(err){
+       return  console.log('Unable to connect to server');
+        
+    }
+    console.log('Connected to Mongo DB Server');
+
+    let db=client.db('TodoApp');
+
+    db.collection('Todos').find({completed:true}).toArray()
+    .then((docs)=>{
+        console.log(JSON.stringify(docs,undefined,2));
+        
+    },(err)=>{
+        console.log(err);
+        
+    })
+    client.close();
+
+
+});
