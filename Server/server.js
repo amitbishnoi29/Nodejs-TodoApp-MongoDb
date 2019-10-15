4d6b0c0a70f4fba63d848c6a9972d473f838416b
unknown <amitbishnoi.brw@gmail.com>
2019-10-15 15:06:14 +0530

    Creating user collecton and adding data to it

--- Server/server.js
+++ Server/server.js
@@ -3,35 +3,61 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp')
 
-let Todo = mongoose.model('Todo',{
-    text: {
-        type: String,
-        required: true,
-        minlength : 1,
-        trim: true
-    },
-    completed: {
-        default : false,
-        type : Boolean
-    },
-    completedAt: {
-        type : Number,
-        default:null
-    }
+// let Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength : 1,
+//         trim: true
+//     },
+//     completed: {
+//         default : false,
+//         type : Boolean
+//     },
+//     completedAt: {
+//         type : Number,
+//         default:null
+//     }
+// })
 
+// let newTodo = new Todo({
+//     text : 'Read Books',
+//     completed:true,
+//     completedAt: 123
+// });
 
+// newTodo.save().then((e) => {
+//     console.log(JSON.stringify(e,undefined,3));
+    
+// },(err) => {
+//     console.log(err);
+    
+// })
+
+// Creating User collection
+
+const User = mongoose.model("User",{
+    name: {
+        type:String,
+        minlength:1,
+        trim:true,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    } 
 })
 
-let newTodo = new Todo({
-    text : 'Read Books',
-    completed:true,
-    completedAt: 123
-});
+const newUser = new User({
+    name : "Andrew",
+    email : "andewmead@gmail.com"
+})
 
-newTodo.save().then((e)=>{
-    console.log(JSON.stringify(e,undefined,3));
-    
-},(err)=>{
-    console.log(err );
-    
+newUser.save().then((res) => {
+    console.log(JSON.stringify(res,undefined,3));   
+}, (e) => {
+    console.log('Error occured');
 })
